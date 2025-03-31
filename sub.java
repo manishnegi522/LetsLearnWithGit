5872fd9c6497a765e4e039043317373e2aef55a2
Manish <manishnegi522@gmail.com>
2025-04-01 00:54:04 +0530

    commit for the updation of code in sub.java

--- sub.java
+++ sub.java
@@ -1,5 +1,16 @@
 package LetsLearnWithGit;
 
 public class sub {
+
+
+    public static void main(String[] args) {
+    
+        int a = 5  ;
+        int b = 4  ;
+        
+        System.out.println(a-b);
+        
+        }
+
     
 }
