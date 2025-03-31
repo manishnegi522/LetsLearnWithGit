c9f833f1c53cdc2ccee5adf1aafeed429df12fb6
Manish <manishnegi522@gmail.com>
2025-03-31 17:45:39 +0530

    in sout i have changes the lit bit of code

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 int a = 5  ;
 int b = 4  ;
 
-System.out.println("here is the answer"+a+b);
+System.out.println("here is the answer for adding above variables"+a+b);
 
 
 
