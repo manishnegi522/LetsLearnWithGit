c7db92320521312663a9aa017fa6152b7f7aae98
Manish <manishnegi522@gmail.com>
2025-04-01 14:57:25 +0530

    first change in dev adding variables x,y,z

--- sum.java
+++ sum.java
@@ -2,14 +2,16 @@ public class sum {
 
 public static void main(String[] args) {
     
-int a = 5  ;
-int b = 4  ;
+// int a = 5  ;
+// int b = 4  ;
 
-System.out.println("here is the answer for adding above variables"+a+b);
+// System.out.println("here is the answer for adding above variables"+a+b);
 
-
-
-
+    
+int x = 5  ;
+int y = 4  ;
+int z = 3 ;
+System.out.println("here is the answer for adding above variables :"  + x+y+z);
 
 }
 
