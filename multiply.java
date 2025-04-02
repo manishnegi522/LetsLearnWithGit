d57510fe0c84fc100f06d88a3d74e732ecd17b8b
Manish <manishnegi522@gmail.com>
2025-04-02 17:07:36 +0530

    files changes in sout in multiply

---
+++ multiply.java
@@ -0,0 +1,17 @@
+public class multiply {
+    
+
+public static void main(String[] args) {
+    
+    int n = 2 ;
+    int m = 5  ;
+    
+    int z = n*m ;
+
+System.out.println("here is the multiplication" + z);
+    
+
+}
+
+
+}
